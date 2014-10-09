8692c752e1eb1ec1ffd8c9a8995e0402a8b8953d
Rachel Sison <rsison87@gmail.com>
2014-10-08 17:34:30 -0700

    Completed [still one error]

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,54 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === null){
+      return "null";
+  }
+  if(obj.constructor === String){
+      return '"' + obj + '"';
+  }
+  if(obj.constructor === Boolean){
+      return '' + obj.toString() + '';
+  }
+  if(obj.constructor === Number) {
+      return obj.toString();
+  }
+  if(obj.constructor === Array){
+      var stringObject = '[';
+      var stringIt = function(obj) {
+        if(obj.length === 0){
+          return '' + stringObject + ']';
+        }else{
+          var comma = ',';
+          if(obj.length === 1){
+              comma = '';
+          }
+          stringObject += '' + stringifyJSON(obj.splice(0, 1)[0]) + comma + '';
+          
+          return stringIt(obj);
+        }
+      }
+      return stringIt(obj);
+  }
+
+  if(obj.constructor === Object){
+      var stringObj = '{';
+      var stringThis = function(obj){
+          var keysList = Object.keys(obj);
+          if(keysList.length === 0){
+              return '' + stringObj + '}' ;
+          }else{
+          comma = ',';
+          if(keysList.length === 1){
+              comma = '';
+          }
+              stringObj += '"' + keysList[0] + '":' + stringifyJSON(obj[keysList[0]]) + comma;
+              delete obj[keysList[0]];
+              return stringThis(obj);
+          }
+      }
+      return stringThis(obj);
+  }
+
+
 };
